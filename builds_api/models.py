9cc6dbb1576b59588f917216b871bf5e0092757c
Alexander Byun <alexanderbyun@AB-MBP16.attlocal.net>
2022-06-20 23:50:14 -0400

    rating added, description textfield, charfield max length 255

--- builds_api/models.py
+++ builds_api/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Post(models.Model):
-    name = models.CharField(max_length=32)
-    description = models.CharField(max_length=255)
-    cpu = models.CharField(max_length=32)
-    cooler = models.CharField(max_length=32)
-    mobo = models.CharField(max_length=32)
-    ram = models.CharField(max_length=32)
-    psu = models.CharField(max_length=32)
-    gpu = models.CharField(max_length=32)
-    storage = models.CharField(max_length=32)
-    case = models.CharField(max_length=32)
-    img = models.CharField(max_length=200)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    cpu = models.CharField(max_length=255)
+    cooler = models.CharField(max_length=255)
+    mobo = models.CharField(max_length=255)
+    ram = models.CharField(max_length=255)
+    psu = models.CharField(max_length=255)
+    gpu = models.CharField(max_length=255)
+    storage = models.CharField(max_length=255)
+    case = models.CharField(max_length=255)
+    img = models.CharField(max_length=255)
+    rating = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(0)])
--- builds_api/serializers.py
+++ builds_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer): 
     class Meta:
         model = Post
-        fields = ('id', 'name', 'description', 'cpu', 'cooler', 'mobo', 'ram', 'psu', 'gpu', 'storage', 'case', 'img',)
+        fields = ('id', 'name', 'description', 'cpu', 'cooler', 'mobo', 'ram', 'psu', 'gpu', 'storage', 'case', 'img', 'rating',)
